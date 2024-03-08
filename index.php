aa2f8ebc000c9ff9a8c109193d84a35f6dd90e23
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-03-08 13:18:32 +0100

    include once

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
   <?php
-    include('partials/header.php');
+    include_once('partials/header.php');
   ?>  
     <main>
       <section class="slides-container">
--- partials/header.php
+++ partials/header.php
@@ -3,19 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/slider.css">
-    <link rel="stylesheet" href="css/form.css">
-    <link rel="stylesheet" href="css/portfolio.css">
-    <link rel="stylesheet" href="css/accordion.css">
-    <link rel="stylesheet" href="css/banner.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Moja stránka</title>
+    <?php
+      $pages = glob('*.php');
+      foreach($pages as $page_path){
+        $page_name = basename($page_path,'.php');
+        echo('<link rel="stylesheet" href="css/style.css">');
+        echo('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+        switch($page_name){
+          case 'index':
+            echo('<link rel="stylesheet" href="css/slider.css">');
+            break;
+          case 'kontakt':
+            echo('<link rel="stylesheet" href="css/banner.css">');
+            echo('<link rel="stylesheet" href="css/form.css">');
+            break;
+          case 'portfolio':
+            echo('<link rel="stylesheet" href="css/portfolio.css">');
+            break;
+          case 'qna':
+            echo('<link rel="stylesheet" href="css/accordion.css">');
+            break;
+        }
+      }
+    ?>
 </head>
 <body>
     <header class="container main-header">
         <div>
-          <a href="index.html">
+          <a href="index.php">
             <img src="img/logo.png" height="40">
           </a>
         </div>
