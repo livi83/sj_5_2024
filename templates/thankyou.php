e5d18cf16bb7d88d1f03da5f008c6149b632b913
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-03-22 14:00:06 +0100

    insert do db

--- templates/thankyou.php
+++ templates/thankyou.php
@@ -8,7 +8,37 @@ include('partials/header.php');
           <div class="col-100 text-center">
               <h2>Ďakujeme za vyplnenie formulára</h2>
               <?php
-                print_r($_POST);
+                //print_r($_POST);
+                $conn = db_connection();
+                if($conn){
+                  //echo 'Mám spojenie';
+                  if(isset($_POST['contact_submitted'])){
+                     // echo 'Formulár bol odoslaný';
+                     $data = array('contact_name'=>$_POST['name'],
+                        'contact_email'=>$_POST['email'],
+                        'contact_message'=>$_POST['message'],
+                        'contact_acceptance'=>$_POST['acceptance'],
+                      );
+
+                      try{
+
+                        $query = "INSERT INTO contact (name, email, message, acceptance) 
+                        VALUES (:contact_name, :contact_email, :contact_message, :contact_acceptance)";
+                        $query_run = $conn->prepare($query);
+                        $query_run->execute($data);    
+  
+                      }catch(PDOException $e){
+                       
+                        echo $e->getMessage();
+                      }
+
+
+                  }else{
+                      echo 'Formulár nebol odoslaný';
+                  }
+                }else{
+                  echo 'Nemám spojenie';
+                }
               ?>
           </div>
         </div>
