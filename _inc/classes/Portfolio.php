27abc7f53a1009da633c61640ad47eb00bb4cdac
Lívia Kelebercová <livia.kelebercova@gmail.com>
2024-04-12 14:17:11 +0200

    qna, portfolio

---
+++ _inc/classes/Portfolio.php
@@ -0,0 +1,24 @@
+<?php
+
+class Portfolio extends Database{
+
+    private $db;
+
+    public function __construct(){
+        $this->db = $this->db_connection();
+    }
+    
+    public function select(){
+        try{
+            $sql = "SELECT * FROM portfolio";
+            $query = $this->db->query($sql);
+            $portfolio = $query->fetchAll();
+            return $portfolio;
+
+        }catch(PDOException $e){
+            echo($e->getMessage());
+        }   
+    }
+}
+
+?>
